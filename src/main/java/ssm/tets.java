11fea88b12cf78b363e710eced5491a697f450a8
ForrstGoahed <shaoht1993@163.com>
2017-12-19 17:59:59 +0800

    Initial commit

---
+++ src/main/java/ssm/tets.java
@@ -0,0 +1,14 @@
+package ssm;
+
+/**
+ * ClassName：
+ * Description：
+ * <p>company：58同城 <br>
+ * Copyright：Copyright ? 2011 58.com All Rights Reserved<br>
+ *
+ * @author shaohongtao
+ * @Date 2017/12/19 17:07
+ * @since JRE 1.6.0_22  or higher
+ */
+public class tets {
+}
